665f2c02252a0f36a1bf9eabe2f7a1b4bd5450a5
adeyemitobiloba <tobilobaadeyemi400@gmail.com>
2024-07-06 23:14:49 +0100

    Add files via upload

---
+++ tech.py
@@ -0,0 +1,15 @@
+username=(input("your name:"))
+my_name=("oluwatobiloba")
+my_bro_name=("anjola")
+my_first_name=("Oluwatobiloba")
+my_bro_first_name=("Anjola")
+if username==my_name:
+    print("your name should begin with a capital letter")
+elif username==my_bro_first_name:
+    print("you are smart")
+elif username==my_first_name:
+    print("you can continue")        
+elif username==my_bro_name: 
+    print("the first letter is not in capital letter")
+else:
+    print("you are allowed to continue")       
